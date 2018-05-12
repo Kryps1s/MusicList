0d073c9b2e98377ef77b0308417b1badd607c946
Elliot O <eoreilly1994@gmail.com>
2018-05-12 14:16:33 -0400

    added redux and framework for state spinner during login processing

--- src/components/account/LoginPage.jsx
+++ src/components/account/LoginPage.jsx
@@ -2,13 +2,39 @@ import React from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export default class LoginPage extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+
+    this.compileFormData = this.compileFormData.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+    // component state
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
-  
+
+  // update state as email values change
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  // update state as password values changes
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value })
+  }
+
+  compileFormData() {
+    const { loginFunction } = this.props;
+    const formData = this.state;
+    loginFunction(formData);
+  }
+
   render() {
     return (
-       <div className="row justify-content-center">
+      <div className="row justify-content-center">
         <div className="col-10 col-sm-7 col-md-5 col-lg-4">
           <Form>
             <FormGroup>
@@ -18,6 +44,8 @@ export default class LoginPage extends React.Component {
                 name="email"
                 id="exampleEmail"
                 placeholder="noreply@musiclist.com"
+                value={this.state.email}
+                onChange={this.handleEmailChange}
               />
             </FormGroup>
             <FormGroup>
@@ -27,9 +55,11 @@ export default class LoginPage extends React.Component {
                 name="password"
                 id="examplePassword"
                 placeholder="password"
+                value={this.state.password}
+                onChange={this.handlePasswordChange}
               />
             </FormGroup>
-            <Button>Log In</Button>
+            <Button onClick={this.compileFormData}>Log In</Button>
           </Form>
         </div>
       </div>
